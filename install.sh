97b08ef1c80b93b668a1dfe30f06eb9c604cf330
lorenzo95 <lorenzo95@users.noreply.github.com>
2016-12-27 20:27:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # iptables configuration for "straker"
 #
 
-echo "** Starting firewall configuration..."
+echo "$(date) - ** Starting firewall configuration..."
 
 PATH=/sbin:/usr/bin:/bin:/usr/local/bin
 
@@ -17,7 +17,7 @@ export PATH
 #
 if [ ! -d $DIR/rules.d ]
 then
-	echo "$0: no such directory: $DIR/rules.d" >&1
+	echo "$(date) - $0: no such directory: $DIR/rules.d" >&1
 	exit 1
 fi
 
@@ -25,24 +25,46 @@ fi
 # Configuration
 #
 
+getip() {
+    ip addr show $1 | grep -Po 'inet \K[\d.]+'
+}
+
+getnet() {
+    ip route | grep  $1 | grep -Po '^[1-2]\S*'
+}
+
+#---------------------------#
+# BPI-R1 VLAN configuration #
+#---------------------------#
+#
+# port ordering (view from front):
+# [ 2 1 0 4 ] [ 3 ]
+# [ eth0.101 eth0.102 eth0.103 eth0.104 ] [ eth0.201 ]
+#
+
 export OUTSIDE_IF=eth0.201
 export OUTSIDE_NET=0.0.0.0/0
-# external IP is dynamic:
-export OUTSIDE_ADDR=`/sbin/ifconfig $OUTSIDE_IF | awk '/inet addr/ { print $2; }' | awk -F: '{ print $2 }'`
+export OUTSIDE_ADDR=$(getip $OUTSIDE_IF)
+
+echo "$(date) - Outside interface $OUTSIDE_IF in network $OUTSIDE_NET has address $OUTSIDE_ADDR"
+
+export INSIDE_IF=eth0.101
+export INSIDE_ADDR=$(getip $INSIDE_IF)
+export INSIDE_NET=$(getnet $INSIDE_IF)
+
+echo "$(date) - Inside interface $INSIDE_IF in network $INSIDE_NET has address $INSIDE_ADDR"
+
+export DMZ_IF=eth0.104
+export DMZ_ADDR=$(getip $DMZ_IF)
+export DMZ_NET=$(getnet $DMZ_IF)
+
+echo "$(date) - DMZ interface $DMZ_IF in network $DMZ_NET has address $DMZ_ADDR"
 
 if [ -z "$OUTSIDE_ADDR" ]
 then
 	echo "WARNING: cannot determine external IP address"
 fi
 
-
-export INSIDE_IF=eth0.101
-export INSIDE_ADDR=192.168.13.5
-export INSIDE_NET=192.168.13.0/24
-
-export DMZ_IF=eth0.104
-export DMZ_ADDR=192.168.14.1
-export DMZ_NET=192.168.14.0/24
 
 ############################################################################
 
@@ -64,16 +86,30 @@ modprobe ip_nat_ftp
 # Set default policies and initial blocking rules
 #
 
+# Disable Forwarding
+if [ -r /proc/sys/net/ipv4/ip_forward ]; then
+  echo "$(date) - Disabling IP forwarding"
+  echo "0" > /proc/sys/net/ipv4/ip_forward
+fi
+
 # set the default policies
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
 # clear the current configuration
+
+# -F for Flush the selected chain (all the chains in the table if none is given). 
+# This is equivalent to deleting all the rules one by one. 
 iptables -F
 iptables -F -t nat
+
+# -Z for Zero the packet and byte counters in all chains
 iptables -Z
 iptables -Z -t nat
+
+# -X for Delete the optional user-defined chain specified. 
+# If no argument is given, it will attempt to delete every non-builtin chain in the table. 
 iptables -X
 
 # add blocking entries at the front of the chains
@@ -81,14 +117,41 @@ iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
 iptables -A FORWARD -j DROP
 
-for r in $DIR/rules.d/*.sh
-do
-	echo "running $r..."
-	$r
-done
+#Dropping all ipv6 traffic
+echo "$(date) - I see no need for ipv6 support"
+ip6tables -P INPUT DROP
+ip6tables -P OUTPUT DROP
+ip6tables -P FORWARD DROP
+
+#for r in $DIR/rules.d/*.sh
+#do
+#	echo "running $r..."
+#	$r
+#done
+
+
+
+
+#
+#
+#
+
+$DIR/rules.d/0-defend.sh
+$DIR/rules.d/1-firewall.sh
+$DIR/rules.d/2-icmp.sh
+$DIR/rules.d/3-outgoing.sh
+
+
+
+#
+#
+#
+
+
 
 # just in case...
 accept_from_inside tcp ssh
+accept_from_outside tcp ssh
 
 #
 # Drop broadcast traffic from the inside before logging
@@ -100,6 +163,10 @@ iptables -A INPUT -p udp -i $INSIDE_IF -d 192.168.13.255/32 -j DROP
 iptables -A INPUT -p udp -i $INSIDE_IF -d 255.255.255.255/32 -j DROP
 
 iptables -A INPUT -p igmp -i $INSIDE_IF -d 224.0.0.1/32 -j DROP
+
+iptables -A INPUT -p udp -i $OUTSIDE_IF -d 255.255.255.255/32 -j DROP
+iptables -A INPUT -p udp -i $OUTSIDE_IF -d 192.168.1.255/32 -j DROP
+
 
 #
 # Log invalid packets separately
@@ -128,6 +195,19 @@ iptables -D INPUT 1
 iptables -D OUTPUT 1
 iptables -D FORWARD 1
 
-echo "** Firewall configuration complete."
 
+
+# Enable Forwarding
+if [ -r /proc/sys/net/ipv4/ip_forward ]; then
+  echo "$(date) - Enabling IP forwarding"
+  echo "1" > /proc/sys/net/ipv4/ip_forward
+fi
+
+echo "$(date) - ** Firewall configuration complete."
+
+echo "$(date) - ###################################" 	>>/var/log/kern.log
+echo "$(date) - ***Firewall reset..."			>>/var/log/kern.log
+echo "$(date) - ###################################"	>>/var/log/kern.log
+
+iptables -L -v
 exit 0
