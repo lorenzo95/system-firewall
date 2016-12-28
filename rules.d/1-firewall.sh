4b77fd58206bbf5f9c797f72c7c84aabd72d9805
lorenzo95 <lorenzo95@users.noreply.github.com>
2016-12-27 20:28:55 -0700

    Update 1-firewall.sh

--- rules.d/1-firewall.sh
+++ rules.d/1-firewall.sh
@@ -6,15 +6,20 @@
 . functions.sh
 
 # allow everything on the loopback interface
-iptables -A INPUT  -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+#iptables -A INPUT  -i lo -j ACCEPT
+#iptables -A OUTPUT -o lo -j ACCEPT
 
 # logging
-allow_to_inside tcp 514
+#allow_to_inside tcp 514
+
+#web for testing
+allow_to_outside tcp http
+allow_to_outside tcp https
 
 # DNS
-allow_to_outside tcp domain
-allow_to_outside udp domain
+#allow_to_outside tcp domain
+#allow_to_outside udp domain
+allow_to_nameserver
 
 # NTP
 allow_to_outside udp ntp
@@ -25,12 +30,12 @@ accept_from_outside udp 68
 
 # DNS
 accept_from_inside udp domain
-accept_from_inside tcp domain
+#accept_from_inside tcp domain
 accept_from_dmz udp domain
-accept_from_dmz tcp domain
+#accept_from_dmz tcp domain
 
 # DNS (temporary)
-allow_to_inside udp domain
+#allow_to_inside udp domain
 
 exit 0
 
