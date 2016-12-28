7f41111cbce2af29e072cf8cc66bf4a591f90292
lorenzo95 <lorenzo95@users.noreply.github.com>
2016-12-27 20:30:16 -0700

    Update 3-outgoing.sh

--- rules.d/3-outgoing.sh
+++ rules.d/3-outgoing.sh
@@ -6,86 +6,87 @@
 . functions.sh
 
 # special dispensation for specific hosts
-forward_host_to_outside 192.168.13.35/32    # Scott's laptop
+forward_host_to_outside tcp $INSIDE_NET    # Inside Network
+forward_host_to_outside udp $INSIDE_NET
 
 # mail
-forward_to_outside tcp smtp
-forward_to_outside tcp pop3
-forward_to_outside tcp pop3s
-forward_to_outside tcp imap
-forward_to_outside tcp imaps
+#forward_to_outside tcp smtp
+#forward_to_outside tcp pop3
+#forward_to_outside tcp pop3s
+#forward_to_outside tcp imap
+#forward_to_outside tcp imaps
 
 # SSH
-forward_to_outside tcp ssh
+#forward_to_outside tcp ssh
 
 # web
-forward_to_outside tcp http
-forward_to_outside tcp https
-forward_to_outside tcp 81
-forward_to_outside tcp 8000
-forward_to_outside tcp 8080
-forward_to_outside tcp 8081
+#forward_to_outside tcp http
+#forward_to_outside tcp https
+#forward_to_outside tcp 81
+#forward_to_outside tcp 8000
+#forward_to_outside tcp 8080
+#forward_to_outside tcp 8081
 
 # Adobe flash security policy
-forward_to_outside tcp 843
+#forward_to_outside tcp 843
 
 # Akamai CDN
-forward_to_outside tcp 1935
+#forward_to_outside tcp 1935
 
 # telnet
-forward_to_outside tcp telnet
+#forward_to_outside tcp telnet
 
 # DNS
-forward_to_outside tcp domain
-forward_to_outside udp domain
+#forward_to_outside tcp domain
+#forward_to_outside udp domain
 
 # NTP
-forward_to_outside tcp ntp
-forward_to_outside udp ntp
+#forward_to_outside tcp ntp
+#forward_to_outside udp ntp
 
 # ms-streaming
-forward_to_outside tcp 1755
+#forward_to_outside tcp 1755
 
 # RTSP / RealPlayer
-forward_to_outside tcp 554
-forward_to_outside tcp 322
+#forward_to_outside tcp 554
+#forward_to_outside tcp 322
 
 # cpanel
-forward_to_outside tcp 2083
+#forward_to_outside tcp 2083
 
 # IJ Cloud Printing service
-forward_to_outside tcp 5222
+#forward_to_outside tcp 5222
 
 # Android market
-forward_to_outside tcp 5228
+#forward_to_outside tcp 5228
 
 # DynDNS query URL
-forward_to_outside tcp 8245
+#forward_to_outside tcp 8245
 
 # Allow Jenny to RDP to uni
-forward_to_outside tcp 3389
+#forward_to_outside tcp 3389
 
 # GIT Repos
-forward_to_outside tcp 9418
+#forward_to_outside tcp 9418
 
 # Pobox SMTP
-forward_to_outside tcp 465
+#forward_to_outside tcp 465
 
 # subversion: svn://
-forward_to_outside tcp 3690
+#forward_to_outside tcp 3690
 
 # Yum repos
-forward_to_outside tcp 4040
+#forward_to_outside tcp 4040
 
 # Apple push notification service
-forward_to_outside tcp 5223 17.0.0.0/8
+#forward_to_outside tcp 5223 17.0.0.0/8
 
 #
 # Stuff from Windows et al.
 #
-forward_to_outside tcp 40001:40046  # MS updates, errors, authentication
-forward_to_outside tcp 12350        # Skype
-forward_to_outside tcp 33033        # Skype
+#forward_to_outside tcp 40001:40046  # MS updates, errors, authentication
+#forward_to_outside tcp 12350        # Skype
+#forward_to_outside tcp 33033        # Skype
 
 #
 # Traffic from Android (Galaxy S7 phone)
@@ -93,8 +94,8 @@ forward_to_outside tcp 33033        # Skype
 # tcp/7275 = User Plane Location protocol
 # udp/443 = Google QUIC protocol
 #
-forward_to_outside tcp 7275
-forward_to_outside udp 443
+#forward_to_outside tcp 7275
+#forward_to_outside udp 443
 
 #
 # Finally, rewrite the source addresses of all outgoing packets
